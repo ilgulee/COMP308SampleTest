5ef161133db4ae2d1fac9520809db3a4b9f35539
ilgulee <iglee2000ca@gmail.com>
2018-03-07 07:42:32 -0500

    Set config and server.js

---
+++ NodeSampleTest/config/env/development.js
@@ -0,0 +1,8 @@
+//Development configuration options
+// 1 - database name
+// 2 - a secret string to sign the session identifier
+module.exports = {
+    db: 'mongodb://localhost/sample-db',
+    sessionSecret: 'myDevelopmentSecret'
+};
+
